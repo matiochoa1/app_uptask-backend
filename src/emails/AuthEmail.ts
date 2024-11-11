527114438d3e2dc977ea07924d91d4edd3bd3f42
Matias <mattiochoax@gmail.com>
2024-11-11 22:10:47 +0100

    Added the link to the confirm account view in the email anchor

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -16,7 +16,7 @@ export class AuthEmail {
 			text: "UpTask - Confirma tu cuenta",
 			html: `<p>Hola ${user.name}, has creado tu cuenta en UpTask, ya casi esta todo listo. Solo falta confirmar tu cuenta</p>
                 <pVisita el siguiente enlace:</p>
-                <a href="">Confirmar cuenta</a>
+                <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
                 <p>E ingresa el codigo: <b>${user.token}</b></p>
                 <p>Este token expira en 10 minutos.</p>
             `,
